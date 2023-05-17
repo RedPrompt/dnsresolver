63451cc86285d42315d12fe80b252134c71961a5
Matheus M. Pupo <matheusmpupo@gmail.com>
2023-05-17 00:33:03 -0300

    Create dnsresolver.go

---
+++ dnsresolver.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+)
+
+type Options struct {
+	Domain				  string				// Domain name to validate DNS
+	DomainsList			string				// File containing list of domains to validate DNS
+}
+
+func main() {
+	options := &Options{}
+
+	flag.StringVar(&options.Domain, "d", "", "Domain name to validate DNS")
+	flag.StringVar(&options.DomainsList, "dl", "", "File containing list of domains to validate DNS")
+	flag.Parse()
+
+	// Declare domains slice
+	var domainSlice []string
+
+	// Get input from stdin
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+		domainSlice = append(domainSlice, sc.Text())
+	}
+
+	// Get domain from Domain flag
+	if options.Domain != "" {
+		domainSlice = append(domainSlice, options.Domain)
+	}
+
+	// Get domains from DomainList flag
+	if options.DomainsList != "" {
+		readFile, err := os.Open(options.DomainsList)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fileScanner := bufio.NewScanner(readFile)
+		fileScanner.Split(bufio.ScanLines)
+
+		for fileScanner.Scan() {
+			domainSlice = append(domainSlice, fileScanner.Text())
+		}
+
+		readFile.Close()
+	}
+
+	// Get each domain from appended domain slice
+	// Resolve DNS for each domain in slice and print if resolved
+	for _, domain := range domainSlice {
+		_, err := net.ResolveIPAddr("ip4", domain)
+		if err == nil {
+			fmt.Println(domain)
+		}
+	}
+	
+}
